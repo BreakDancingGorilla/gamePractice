a69f7bcfc2c44473bd69cd32d7e756a2b50f01ef
BreakDancingGorilla <gabrielosborn1029@gmail.com>
2026-06-06 11:44:10 -0400

    fallout autosave type shit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
+import {player} from './player.js';
+import {input} from './input.js';
+
 const canvas = document.getElementById('gameCanvas');
 export const ctx = canvas.getContext('2d');
+
+
 
 canvas.width = 1000;
 canvas.height = 500;
--- js/player.js
+++ js/player.js
@@ -1,10 +1,11 @@
 import { ctx } from './main.js';
 
 class player {
-    constructor(x = ctx.width/2, y = ctx.height/2, width = 50, height = 50, color = '#00f5ff') {
+    constructor(x = ctx.width/2, y = ctx.height/2, width = 50, height = 50, color = '#00f5ff', speed = 5) {
         this.x = x;
         this.y = y;
         this.width = width;
+        this.speed = speed;
         this.height = height;
         this.color = color;
     }
@@ -13,4 +14,20 @@ class player {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
+
+    update(keypressed) {
+        if (keypressed['ArrowLeft']) {
+            this.x -= this.speed;
+        }
+        if (keypressed['ArrowRight']) {
+            this.x += this.speed;
+        }
+        if (keypressed['ArrowUp']) {
+            this.y -= this.speed;
+        }
+        if (keypressed['ArrowDown']) {
+            this.y += this.speed;
+        }
+    }
+    
 }
